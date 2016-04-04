5b79b1b037f9368b2941e72c06232a4828dd773d
Julian Palacios Goerger <julian202@gmail.com>
2016-04-04 16:41:34 -0500

    Added  to manual.cs :   if (Ref0V == 0) Ref0V = 2000;       if (Ref2V == 0) Ref2V = 14000;

--- COMMS.cs
+++ COMMS.cs
@@ -447,6 +447,8 @@ namespace LukMachine
       Properties.Settings.Default.RefCount2V = COMMS.Instance.get2v();
       int Ref0V = Convert.ToInt32(Properties.Settings.Default.ground);
       int Ref2V = Properties.Settings.Default.RefCount2V;
+      if (Ref0V == 0) Ref0V = 2000;
+      if (Ref2V == 0) Ref2V = 14000;
       Properties.Settings.Default.RefCount10V = Ref0V + Convert.ToInt32((Ref2V - Ref0V) * 4.75);// or 5;  4.75 is 9.5/2 becuase 9.5V is the voltage when it is empty
       Properties.Settings.Default.Save();
     }
